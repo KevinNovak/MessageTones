4335f601ed70b5434cd1f56c5890f1d43b5e6e7e
Kevin Novak <kevinmnovak@yahoo.com>
2016-01-16 16:06:39 -0500

    Works for /r

--- src/me/kevinnovak/messagetones/MessageTones.java
+++ src/me/kevinnovak/messagetones/MessageTones.java
@@ -1,5 +1,6 @@
 package me.kevinnovak.messagetones;
 
+import java.io.File;
 import java.io.IOException;
 
 import org.bukkit.Bukkit;
@@ -7,6 +8,8 @@ import org.bukkit.ChatColor;
 import org.bukkit.Sound;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -14,11 +17,20 @@ import org.bukkit.event.player.PlayerCommandPreprocessEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class MessageTones extends JavaPlugin implements Listener{
+    public File replyFile = new File(getDataFolder()+"/replies.yml");
+    public FileConfiguration replyData = YamlConfiguration.loadConfiguration(replyFile);
+    
     // ======================
     // Enable
     // ======================
     public void onEnable() {
         saveDefaultConfig();
+        try {
+            replyData.save(replyFile);
+        } catch (IOException e1) {
+            // TODO Auto-generated catch block
+            e1.printStackTrace();
+        }
         Bukkit.getServer().getPluginManager().registerEvents(this, this);
         if (getConfig().getBoolean("metrics")) {
             try {
@@ -38,6 +50,7 @@ public class MessageTones extends JavaPlugin implements Listener{
     // Disable
     // ======================
     public void onDisable() {
+        replyFile.delete();
         Bukkit.getServer().getLogger().info("[MessageTones] Plugin Disabled!");
     }
     
@@ -68,16 +81,52 @@ public class MessageTones extends JavaPlugin implements Listener{
         if (args.length >= 2){
             String cmd = args[0];
             String target = args[1];
+            // =========================
+            // /msg
+            // =========================
             if (cmd.equals("/msg") || cmd.equals("/m") || cmd.equals("/t") || cmd.equals("/tell") || cmd.equals("/whisper") || cmd.equals("/w")) {
                 if (target.length() > 1) {
                     Player targetPlayer = findPlayer(target);
                     if (targetPlayer == null) {
                         return;
                     } else {
+                        replyData.set(targetPlayer.getName() + ".Reply", e.getPlayer().getName());
+                        try {
+                            replyData.save(replyFile);
+                        } catch (IOException e1) {
+                            // TODO Auto-generated catch block
+                            e1.printStackTrace();
+                        }
                         targetPlayer.playSound(targetPlayer.getLocation(),Sound.NOTE_PLING,1,0);
                         return;
                     }
                 }
+            }
+            // =========================
+            // /r
+            // =========================
+            if (cmd.equals("/r") || cmd.equals("/reply")) {
+                // if send command player exists in replies.yml file
+                if (replyData.getString(e.getPlayer().getName() + ".Reply") == null) {
+                    return;
+                }
+                // if reply to player exists on server
+                Player targetPlayer = Bukkit.getServer().getPlayer(replyData.getString(e.getPlayer().getName() + ".Reply"));
+                if (targetPlayer == null) {
+                    return;
+                }
+                // save a new reply to player
+                e.getPlayer().sendMessage(targetPlayer.getName());
+                replyData.set(targetPlayer.getName() + ".Reply", e.getPlayer().getName());
+                try {
+                    replyData.save(replyFile);
+                } catch (IOException e1) {
+                    // TODO Auto-generated catch block
+                    e1.printStackTrace();
+                }
+                // play sound
+                targetPlayer.playSound(targetPlayer.getLocation(),Sound.NOTE_PLING,1,0);
+                return;
             }
         }
     }
