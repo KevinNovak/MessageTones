4374d7783d73b397d8b91246e8e84533c074d445
Kevin Novak <kevinmnovak@yahoo.com>
2016-01-16 00:34:23 -0500

    Setup a Template

---
+++ src/me/kevinnovak/messagetones/MessageTones.java
@@ -0,0 +1,61 @@
+package me.kevinnovak.messagetones;
+
+import java.io.IOException;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class MessageTones extends JavaPlugin implements Listener{
+    // ======================
+    // Enable
+    // ======================
+    public void onEnable() {
+        saveDefaultConfig();
+        Bukkit.getServer().getPluginManager().registerEvents(this, this);
+        if (getConfig().getBoolean("metrics")) {
+            try {
+                MetricsLite metrics = new MetricsLite(this);
+                metrics.start();
+                Bukkit.getServer().getLogger().info("[MessageTones] Metrics Enabled!");
+            } catch (IOException e) {
+                Bukkit.getServer().getLogger().info("[MessageTones] Failed to Start Metrics.");
+            }
+        } else {
+            Bukkit.getServer().getLogger().info("[MessageTones] Metrics Disabled.");
+        }
+        Bukkit.getServer().getLogger().info("[MessageTones] Plugin Enabled!");
+    }
+    
+    // ======================
+    // Disable
+    // ======================
+    public void onDisable() {
+        Bukkit.getServer().getLogger().info("[MessageTones] Plugin Disabled!");
+    }
+    
+    // =========================
+    // Convert String in Config
+    // =========================
+    String convertedLang(String toConvert) {
+        return ChatColor.translateAlternateColorCodes('&', getConfig().getString(toConvert));
+    }
+    
+    // ======================
+    // Commands
+    // ======================
+    public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args) {
+        // ======================
+        // Console
+        // ======================
+        if (!(sender instanceof Player)) {
+            sender.sendMessage(convertedLang("noconsole"));
+            return true;
+        }
+        return true;
+    }
+}
